6314356deac99c7fc9621fdc5e900ccde03b2afb
Samantha brake <sammy.brake@gmail.com>
2019-03-02 16:51:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,8 +16,6 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
     @number = params[:number]
     @phrase = params[:phrase]
-    @number.to_i times do 
-      puts "#{@phrase}"
-    end 
+    
   end 
 end
